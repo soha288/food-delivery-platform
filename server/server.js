700734368b2cda3dca76cbfa88841637975bd0e6
soha288 <sohabegum54@gmail.com>
2026-06-18 21:59:32 +0530

    feat: connect mongodb atlas

--- server/server.js
+++ server/server.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 
 const app = require('./app')
 
-const PORT = process.env.PORT || 5000
+const connectDB =
+require('./config/db')
+
+connectDB()
+
+const PORT =
+process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
