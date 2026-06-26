74260db8c8d1d08d1cd8f84196777a91d829a585
soha288 <sohabegum54@gmail.com>
2026-06-26 17:13:14 +0530

    feat: create order schema

---
+++ server/models/order.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+
+    items: [
+      {
+        menuItem: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Menu',
+          required: true
+        },
+
+        quantity: {
+          type: Number,
+          required: true
+        }
+      }
+    ],
+
+    totalAmount: {
+      type: Number,
+      required: true
+    },
+
+    deliveryAddress: {
+      type: String,
+      required: true
+    },
+
+    paymentMethod: {
+      type: String,
+      enum: ['Cash', 'UPI', 'Card'],
+      default: 'Cash'
+    },
+
+    status: {
+      type: String,
+      enum: [
+        'Pending',
+        'Preparing',
+        'Out for Delivery',
+        'Delivered'
+      ],
+      default: 'Pending'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports =
+mongoose.model(
+  'Order',
+  orderSchema
+)
