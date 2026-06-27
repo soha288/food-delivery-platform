39daf1414b15cddad1d03c481cd2e3ce882bc101
soha288 <sohabegum54@gmail.com>
2026-06-27 20:54:56 +0530

    feat: configure routing and create frontend pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Restaurants from './pages/Restaurants'
+import Menu from './pages/Menu'
+import Cart from './pages/Cart'
+import Orders from './pages/Orders'
+
 function App() {
   return (
-    <div>
-      <h1>Food Delivery Platform</h1>
-      <p>Frontend setup successful 🚀</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/restaurants" element={<Restaurants />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/orders" element={<Orders />} />
+    </Routes>
   )
 }
 
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>Welcome to Food Delivery Platform</p>
+    </div>
+  )
+}
+
+export default Login
