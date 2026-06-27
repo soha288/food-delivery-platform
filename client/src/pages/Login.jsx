3888616968c8c0122db8b67704a5936d9dc092f9
soha288 <sohabegum54@gmail.com>
2026-06-27 21:00:25 +0530

    feat: configure tailwind css and create login ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,33 @@
 function Login() {
   return (
-    <div>
-      <h2>Login Page</h2>
-      <p>Welcome to Food Delivery Platform</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+
+      <div className="bg-white shadow-lg rounded-xl p-8 w-96">
+
+        <h1 className="text-3xl font-bold text-center text-orange-600 mb-6">
+          Food Delivery
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border rounded-lg p-3 mb-4"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full border rounded-lg p-3 mb-6"
+        />
+
+        <button
+          className="w-full bg-orange-500 text-white py-3 rounded-lg hover:bg-orange-600"
+        >
+          Login
+        </button>
+
+      </div>
+
     </div>
   )
 }
