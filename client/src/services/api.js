fa149fb60f6bf53f99c8a1d3676f5a25c9b03be8
soha288 <sohabegum54@gmail.com>
2026-07-17 23:17:00 +0530

    frontend deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://food-delivery-platform-ej8n.onrender.com/'
 });
 
 api.interceptors.request.use(
